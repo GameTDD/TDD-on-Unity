368785a7f6d97d9a636301481f3a1d2704b9c500
naomijub <jnaomi@thoughtworks.com>
2019-07-25 22:38:51 -0300

    No test script

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    public float speed = 20f;
+    Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        float x = h * speed * Time.deltaTime;
+        float z = v * speed * Time.deltaTime;
+
+        _rb.MovePosition(transform.position + new Vector3(x, 0, z));
+    }
+}
